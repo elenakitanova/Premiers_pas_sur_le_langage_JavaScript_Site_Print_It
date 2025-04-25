8f49f0fe3b7edcf6649ba7056e114aa354805c6a
Elena Kitanova <kitanovaelena@gmail.com>
2025-04-25 11:38:03 +0200

    rajout javascript

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,59 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+<<<<<<< HEAD
+=======
+
+// Rajout complémentaire 
+
+//Création d'une variable d’index
+let currentIndex = 0;
+
+// Sélections DOM des éléments HTML à modifier
+const bannerImage = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+const dotsContainer = document.querySelector(".dots");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+// Création dynamique des bullet points ajout de boucle pour créer 1 dot par slide
+slides.forEach((slide, index) => {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+	if (index === 0) dot.classList.add("dot_selected");
+	dotsContainer.appendChild(dot);
+});
+
+//Gérer le clic sur la flèche gauche
+arrowLeft.addEventListener("click", () => {
+	currentIndex--;
+	if (currentIndex < 0) {
+		currentIndex = slides.length - 1; // boucle arrière
+	}
+	updateSlide(currentIndex);
+});
+
+
+//Gérer le clic sur la flèche droite
+arrowRight.addEventListener("click", () => {
+	currentIndex++;
+	if (currentIndex >= slides.length) {
+		currentIndex = 0; // boucle
+	}
+	updateSlide(currentIndex);
+});
+
+//Fonction pour mettre à jour le slider. Création d'une fonction updateSlide(index)
+function updateSlide(index) {
+	// mise à jour de l’image
+	bannerImage.src = `./assets/images/slideshow/${slides[index].image}`; 	// mise à jour du texte
+	bannerText.innerHTML = slides[index].tagLine; // mise à jour du texte
+
+	const dots = document.querySelectorAll(".dot"); // mise à jour des bullets
+	dots.forEach(dot => dot.classList.remove("dot_selected"));
+	dots[index].classList.add("dot_selected");
+}
+
+updateSlide(currentIndex); // Affiche le premier slide au démarrage
+
+>>>>>>> 29c3fec (Premier commit - projet Print It)
