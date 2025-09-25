6f2c6bd50ba094f6df7c2a238de3ac317724e240
Elena Kitanova <kitanovaelena@gmail.com>
2025-09-25 14:50:55 +0200

    Finalisation du carrousel et résolution du conflit de fusion

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image":"slide1.jpg",
+        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image":"slide2.jpg",
+        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image":"slide3.jpg",
+        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image":"slide4.png",
+        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ]
 
 
@@ -32,41 +32,40 @@ const arrowRight = document.querySelector(".arrow_right");
 
 // Création dynamique des bullet points ajout de boucle pour créer 1 dot par slide
 slides.forEach((slide, index) => {
-	const dot = document.createElement("div");
-	dot.classList.add("dot");
-	if (index === 0) dot.classList.add("dot_selected");
-	dotsContainer.appendChild(dot);
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    if (index === 0) dot.classList.add("dot_selected");
+    dotsContainer.appendChild(dot);
 });
 
 //Gérer le clic sur la flèche gauche
 arrowLeft.addEventListener("click", () => {
-	currentIndex--;
-	if (currentIndex < 0) {
-		currentIndex = slides.length - 1; // boucle arrière
-	}
-	updateSlide(currentIndex);
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1; // boucle arrière
+    }
+    updateSlide(currentIndex);
 });
 
 
 //Gérer le clic sur la flèche droite
 arrowRight.addEventListener("click", () => {
-	currentIndex++;
-	if (currentIndex >= slides.length) {
-		currentIndex = 0; // boucle
-	}
-	updateSlide(currentIndex);
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0; // boucle
+    }
+    updateSlide(currentIndex);
 });
 
 //Fonction pour mettre à jour le slider. Création d'une fonction updateSlide(index)
 function updateSlide(index) {
-	// mise à jour de l’image
-	bannerImage.src = `./assets/images/slideshow/${slides[index].image}`; 	// mise à jour du texte
-	bannerText.innerHTML = slides[index].tagLine; // mise à jour du texte
+    // mise à jour de l’image
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`;   // mise à jour du texte
+    bannerText.innerHTML = slides[index].tagLine; // mise à jour du texte
 
-	const dots = document.querySelectorAll(".dot"); // mise à jour des bullets
-	dots.forEach(dot => dot.classList.remove("dot_selected"));
-	dots[index].classList.add("dot_selected");
+    const dots = document.querySelectorAll(".dot"); // mise à jour des bullets
+    dots.forEach(dot => dot.classList.remove("dot_selected"));
+    dots[index].classList.add("dot_selected");
 }
 
 updateSlide(currentIndex); // Affiche le premier slide au démarrage
-
